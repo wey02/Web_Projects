1de1275b4b4821bd2b757b030b975b432c8a19f7
wey02 <168517477+wey02@users.noreply.github.com>
2024-05-12 22:10:36 +0800

    Add files via upload

---
+++ blogr-landing-page-main/index.js
@@ -0,0 +1,11 @@
+function show(){
+    document.querySelector('.nav-links').classList.toggle('active')
+    document.querySelector('.openMenu').classList.toggle('hide')
+    document.querySelector('.closeMenu').classList.toggle('show')
+    document.querySelector('.links').classList.toggle('flex')
+}   document.querySelector('.infos').classList.toggle('show')
+
+function showLinks(){
+    document.querySelector('.connect').classList.toggle('down')
+    document.querySelector('.infos').classList.toggle('flex')
+    }
